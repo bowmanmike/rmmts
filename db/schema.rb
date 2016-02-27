05f55d926c867af60a2eb7a65fdc3d72e9597f4d
Mike Bowman <mbowman2@gmail.com>
2016-02-27 16:17:46 -0500

    Add house_id to mate model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227202434) do
+ActiveRecord::Schema.define(version: 20160227211451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160227202434) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "house_id"
   end
 
   add_index "mates", ["email"], name: "index_mates_on_email", unique: true, using: :btree
