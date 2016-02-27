ba04b92e57eb48b21dfb46738600d52f2a9728e3
karenjho <hokarenj@gmail.com>
2016-02-27 16:18:49 -0500

    Write Chores controller methods

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,8 @@
 class Chore < ActiveRecord::Base
   belongs_to :house
   belongs_to :mate
+
+  validates :name, presence: true
+  validates :frequency, numericality: {only_integer: true}
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :houses do
-    resources :chores, only: [:new, :create, :show, :edit, :update, :destroy]
+    resources :chores, except: [:index]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
