c81a132fe1a60687788f803da9f5b2b8af2bec80
Patrick Cruikshank <patrick.cruikshank@gmail.com>
2016-02-27 16:44:27 -0500

    can create a user, login and logout

--- app/models/mate.rb
+++ app/models/mate.rb
@@ -1,5 +1,10 @@
 class Mate < ActiveRecord::Base
   authenticates_with_sorcery!
+  validates :username, presence: true, uniqueness: true
+  validates :email, uniqueness: true
+  validates_length_of :password, minimum: 3, message: "password must be at least 3 characters long", if: :password
+  validates_confirmation_of :password, message: "should match confirmation", if: :password
+
   belongs_to :house
   has_many :chores
   has_many :created_houses, class_name: House, foreign_key: 'creator_id'
