5d7e152c70dcadf376ad1f0f75e0ab3bb6338503
Mike Bowman <mbowman2@gmail.com>
2016-02-27 16:10:02 -0500

    Create house#index action and views

--- app/controllers/house_controller.rb
+++ app/controllers/house_controller.rb
@@ -1,2 +1,27 @@
 class HouseController < ApplicationController
+
+  def new
+    @house = House.new
+  end
+
+  def create
+  end
+
+  def index
+    @houses = House.all
+  end
+
+  def show
+    @house = House.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
