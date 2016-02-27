83d731f57ed3639bcb3f1f2eceeefa03f5435504
Mike Bowman <mbowman2@gmail.com>
2016-02-27 17:03:40 -0500

    Update create action in house controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -14,6 +14,7 @@ class HousesController < ApplicationController
 
   def create
     @house = House.new(house_params)
+    @house.creator_id = current_user
 
     if @house.save
       redirect_to house_path(@house), notice: "House created!"
