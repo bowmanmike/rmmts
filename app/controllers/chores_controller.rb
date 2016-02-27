252b18b2885f870a3282c503288eee26edc2cf07
karenjho <hokarenj@gmail.com>
2016-02-27 15:55:04 -0500

    Generate Chores controller and write Chores routes

---
+++ app/controllers/chores_controller.rb
@@ -0,0 +1,25 @@
+class ChoresController < ApplicationController
+  def new
+    #code
+  end
+
+  def create
+    #code
+  end
+
+  def show
+    #code
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
+end
