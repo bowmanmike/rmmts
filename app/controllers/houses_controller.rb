a4f51a9614c192e4669036af4bc911935ffadf41
Mike Bowman <mbowman2@gmail.com>
2016-02-27 17:02:17 -0500

    Add house#create action

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,19 +1,25 @@
 class HousesController < ApplicationController
 
-  def new
-    @house = House.new
-  end
-
-  def create
-    @house = House.new
-  end
-
   def index
     @houses = House.all
   end
 
   def show
     @house = House.find(params[:id])
+  end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.new(house_params)
+
+    if @house.save
+      redirect_to house_path(@house), notice: "House created!"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -23,6 +29,11 @@ class HousesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def house_params
+    params.require(:house).permit(:name)
   end
 
 end
