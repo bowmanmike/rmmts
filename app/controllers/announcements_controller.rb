85e477fcaa0ba92d809dd9163337d8e71bafe049
Mike Bowman <mbowman2@gmail.com>
2016-02-28 15:27:47 -0500

    Create announcements and they show up on the house show page

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,2 +1,46 @@
 class AnnouncementsController < ApplicationController
+  before_action :load_house, only: [:new, :create, :edit, :update]
+
+  def index
+    @announcements = Announcement.where(house_id: params[:house_id])
+  end
+
+  def show
+    @announcement = Announcement.find(params[:id])
+  end
+
+  def new
+    @announcement = Announcement.new
+  end
+
+  def create
+    @announcement = @house.announcements.build(announcement_params)
+    @announcement.mate = current_user
+
+    if @announcement.save
+      redirect_to house_path(@house), notice: "Announcement added to house"
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def announcement_params
+    params.require(:announcement).permit(:title, :body)
+  end
+
+  def load_house
+    @house = House.find(params[:house_id])
+  end
+
 end
