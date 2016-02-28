2afa601f609f0ffbbd55142c7ac21ef6f8c57477
karenjho <hokarenj@gmail.com>
2016-02-28 14:55:11 -0500

    Write methods for new and create routes in Expense controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,8 +1,20 @@
 class ExpensesController < ApplicationController
+
+  before_action :load_house
+
   def new
+    @expense = Expense.new
   end
 
   def create
+    @expense = @house.expenses.build(expense_params)
+
+    if @expense.save
+      redirect_to house_path(@house)
+      flash[:notice] = "Expense has been added!"
+    else
+      render :new
+    end
   end
 
   def show
@@ -15,5 +27,15 @@ class ExpensesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:name, :description, :due_date, :amount)
+  end
+
+  def load_house
+    @house = House.find(params[:house_id])
   end
 end
