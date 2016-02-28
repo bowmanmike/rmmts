481cb71406a2a8621d30d4a64aea5909d16147fa
karenjho <hokarenj@gmail.com>
2016-02-28 16:08:05 -0500

    Add methods for amount_paid and amount_owed in Expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,12 @@
 class Expense < ActiveRecord::Base
   belongs_to :house
   has_many :payments
+
+  def amount_paid
+    paid = self.payments.sum(:amount)
+  end
+
+  def amount_owed
+    owed = self.amount - self.amount_paid
+  end
 end
