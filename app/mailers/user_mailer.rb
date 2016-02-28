7084516574c28a80e5fc3fc648e7f55e484f3bbe
Mike Bowman <mbowman2@gmail.com>
2016-02-28 14:05:08 -0500

    Create html and text email, fill in text view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
+    @homepage = houses_url
+    @newhouse = new_house_url
     mail(to: @user.email, subject: "Welcome to rmmts!")
   end
 
