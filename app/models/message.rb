48addfa342d1f57916e07f72d709597318149305
Mike Bowman <mbowman2@gmail.com>
2016-02-29 15:36:10 -0500

    Create Message model

---
+++ app/models/message.rb
@@ -0,0 +1,10 @@
+class Message < ActiveRecord::Base
+  belongs_to :conversation
+  belongs_to :mate
+
+  validates_presence_of :body, :conversation_id, :mate_id
+
+  def message_time
+    created_at.strftime'(%A, %d %b at %Y %l:%M %p')
+  end
+end
