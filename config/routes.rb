10740d5079304e648cbd4d5101754d96c81875b6
Mike Bowman <mbowman2@gmail.com>
2016-02-29 16:18:22 -0500

    Add conversation and resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'houses#index'
-  
+
   resources :houses do
     resources :chores, except: [:index]
     resources :announcements
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   get 'login'   => 'sessions#new',    as: :login
   post 'logout' => 'sessions#destroy', as: :logout
+
+  resources :conversations do
+    resources :messages
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
