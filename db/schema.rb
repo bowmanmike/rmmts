52196cee214752da32412ee400537ca785c33113
Mike Bowman <mbowman2@gmail.com>
2016-02-29 16:07:53 -0500

    Move read column from conversations to messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229204059) do
+ActiveRecord::Schema.define(version: 20160229210540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 20160229204059) do
     t.integer  "receiver_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "read",        default: false
   end
 
   create_table "expenses", force: :cascade do |t|
@@ -94,6 +93,7 @@ ActiveRecord::Schema.define(version: 20160229204059) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "read",            default: false
   end
 
   add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
