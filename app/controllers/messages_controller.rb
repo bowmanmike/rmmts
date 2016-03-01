2f43237f31073678b1c8325ce24555f05c95556e
Mike Bowman <mbowman2@gmail.com>
2016-03-01 15:54:29 -0500

    Tweak to messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -24,6 +24,6 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:body, :mate_id)
+    params.require(:message).permit(:body)
   end
 end
