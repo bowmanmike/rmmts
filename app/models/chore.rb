d594df2afe39bf1eb062466ad5cbc38312dfac97
Mike Bowman <mbowman2@gmail.com>
2016-03-03 16:02:52 -0500

    Add validation to chore due_date

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -6,6 +6,7 @@ class Chore < ActiveRecord::Base
   validates :name, presence: true
   validates :frequency_integer, numericality: {only_integer: true}
   validates :frequency_unit, presence: true
+  validates :due_date, presence: true
 
   def check_status
     return if !self.recurring?
