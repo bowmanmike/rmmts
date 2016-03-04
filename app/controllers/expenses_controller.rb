bd27a0fc5d09737a113c36cc5758f6c7d3933100
karenjho <hokarenj@gmail.com>
2016-03-04 15:38:01 -0500

    Write show, edit, update, destroy methods in Expense controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,9 @@
 class ExpensesController < ApplicationController
 
+  before_filter :require_login
+  
   before_action :load_house
+  before_action :load_expense, only: [:show, :edit, :update, :destroy]
 
   def new
     @expense = Expense.new
@@ -11,35 +14,47 @@ class ExpensesController < ApplicationController
 
     if @expense.save
       redirect_to house_path(@house)
+      flash[:notice] = "Expense has been added!"
     else
       render :new
     end
   end
 
   def show
-    #code
   end
 
   def edit
-    #code
   end
 
   def update
-    #code
+    @expense.update_attributes(expense_params)
+
+    if @expense.save
+      redirect_to house_path(@house)
+      flash[:notice] = "Expense has been updated!"
+    else
+      render :edit
+    end
   end
 
   def destroy
-    #code
+    @expense.destroy
+
+    redirect_to house_path(@house)
+    flash[:notice] = "Expense has been deleted!"
   end
 
   private
 
-  def expense_params
-    params.require(:expense).permit(:name, :description, :amount, :paid, :due_date, :frequency_integer, :frequency_unit, :recurring)
+  def load_expense
+    @expense = Expense.find(params[:id])
   end
 
   def load_house
     @house = House.find(params[:house_id])
   end
 
+  def expense_params
+    params.require(:expense).permit(:name, :description, :amount, :paid, :due_date, :frequency_integer, :frequency_unit, :recurring)
+  end
 end
