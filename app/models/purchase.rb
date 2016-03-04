de2d991969e0e44aaafbc69d84eee9723ab5b2d6
karenjho <hokarenj@gmail.com>
2016-03-03 23:40:38 -0500

    Display different Purchase summary by user on show view

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -5,30 +5,38 @@ class Purchase < ActiveRecord::Base
   mount_uploader :image, BillUploader
 
   def amount_paid
-    paid = self.payments.sum(:amount) + self.amount_for_each_mate
+    paid = self.payments.sum(:amount)
   end
 
   def amount_owed
-    owed = self.amount - self.amount_paid
+    owed = self.amount - self.amount_paid - self.amount_for_each
   end
 
-  def amount_for_each_mate
+  def amount_for_each
     each = ( self.amount / self.mate.house.mates.size ).round(2)
   end
 
-  def payments_paid_by_mate(housemate)
-    housemate_payments = self.payments.where(id: housemate.id)
+  def grouped_payments
+    grouped_payments = self.payments.group(:mate_id).sum(:amount)
   end
 
-  def amount_paid_by_mate(housemate)
-    housemate_paid = self.payments_paid_by_mate(housemate).sum(:amount)
+  def payments_paid_by(housemate)
+    housemate_payments = self.payments.where(mate_id: housemate.id)
   end
 
-  def amount_owed_by_mate(housemate)
-    housemate_owes = self.amount_for_each_mate - self.amount_paid_by_mate(housemate)
+  def amount_paid_by(housemate)
+    housemate_paid = self.payments_paid_by(housemate).sum(:amount)
+  end
+
+  def amount_owed_by(housemate)
+    housemate_owes = self.amount_for_each - self.amount_paid_by(housemate)
   end
 
   def fully_paid?
     self.amount_paid >= self.amount
+  end
+
+  def paid_by?(housemate)
+    self.amount_paid_by(housemate) >= self.amount_for_each
   end
 end
