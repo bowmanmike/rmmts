8922df870747ad2bbfeb10422187ccfd8c9a7ec7
karenjho <hokarenj@gmail.com>
2016-03-04 14:51:20 -0500

    Generate Expense controller and define routes

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,27 @@
+class ExpensesController < ApplicationController
+
+  def new
+    #code
+  end
+
+  def create
+    #code
+  end
+
+  def show
+    #code
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
+
+end
