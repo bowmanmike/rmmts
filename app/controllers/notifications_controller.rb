74ff770d80590c41bbe04dd190c0ae998c96a722
Mike Bowman <mbowman2@gmail.com>
2016-03-04 19:39:06 -0500

    Add notification preferences to chore partial

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,26 @@
 class NotificationsController < ApplicationController
+  before_action :load_chore
+
+  def update
+    @notification = Notification.find(params[:id])
+
+    if @notification.update_attributes(notification_params)
+      flash[:notice] = "Notification preferences updated"
+      redirect_to house_path(@chore.house)
+    else
+      flash[:alert] = "There was a problem saving your notification preferences. Please try again."
+      redirect_to house_path(@chore.house)
+    end
+  end
+
+  private
+
+  def notification_params
+    params.require(:notification).permit(:sms, :email)
+  end
+
+  def load_chore
+    @chore = Notification.find(params[:id]).chore
+  end
+
 end
