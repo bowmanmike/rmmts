d90a43bf6a287030d83bd9a20cd85487b6d9eda9
Mike Bowman <mbowman2@gmail.com>
2016-03-05 14:18:07 -0500

    Update schema file from database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305163816) do
+ActiveRecord::Schema.define(version: 20160304224855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,14 +36,13 @@ ActiveRecord::Schema.define(version: 20160305163816) do
     t.boolean  "complete"
     t.integer  "house_id"
     t.integer  "mate_id"
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.integer  "creator_id"
-    t.boolean  "recurring",              default: true
+    t.boolean  "recurring",           default: true
     t.string   "frequency_unit"
     t.integer  "reminder_id"
     t.integer  "due_notification_id"
-    t.integer  "update_due_date_job_id"
   end
 
   add_index "chores", ["creator_id"], name: "index_chores_on_creator_id", using: :btree
@@ -85,9 +84,8 @@ ActiveRecord::Schema.define(version: 20160305163816) do
     t.integer  "house_id"
     t.integer  "reminder_id"
     t.integer  "due_notification_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "update_due_date_job_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
   end
 
   add_index "expenses", ["house_id"], name: "index_expenses_on_house_id", using: :btree
