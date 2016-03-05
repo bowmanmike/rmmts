142adba7a70993b1c0986b4f59bda9a171f6ae18
Mike Bowman <mbowman2@gmail.com>
2016-03-05 17:08:28 -0500

    add phone number, update gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305163816) do
+ActiveRecord::Schema.define(version: 20160305215312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20160305163816) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "house_id"
+    t.integer  "phone_number"
   end
 
   add_index "mates", ["email"], name: "index_mates_on_email", unique: true, using: :btree
