514ffc8d61aadfb8296178f3ed0aea41553ed992
Mike Bowman <mbowman2@gmail.com>
2016-03-05 16:07:50 -0500

    Add notification preferences to group notification job

--- app/jobs/group_chore_reminder_job.rb
+++ app/jobs/group_chore_reminder_job.rb
@@ -3,7 +3,12 @@ class GroupChoreReminderJob < ActiveJob::Base
 
   def perform(chore)
     chore.house.mates.each do |mate|
-      MateMailer.chore_notification(chore, mate).deliver_later
+      notification = mate.notifications.find_by(chore_id: chore.id)
+      if notification.email?
+        MateMailer.chore_notification(chore, mate).deliver_later
+      elsif notification.sms?
+        puts "Sending SMS"
+      end
     end
   end
 
