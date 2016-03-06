703fb6ad47dd553e19057e99fce88b0960f2a5e0
Patrick Cruikshank <patrick.cruikshank@gmail.com>
2016-03-05 19:41:28 -0500

    post, edit, delete announcements handled via AJAX

--- app/jobs/group_chore_reminder_job.rb
+++ app/jobs/group_chore_reminder_job.rb
@@ -3,7 +3,7 @@ class GroupChoreReminderJob < ActiveJob::Base
 
   def perform(chore)
     chore.house.mates.each do |mate|
-      MateMailer.chore_notification(chore, mate).deliver_later
+      MateMailer.chore_notification(chore, mate).deliver_later 
     end
   end
 
