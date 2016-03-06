206a3be100d0145cb5ed8b08e1e989d55bdee21a
Mike Bowman <mbowman2@gmail.com>
2016-03-06 11:34:34 -0500

    Uncomment email notifications in group reminder job

--- app/jobs/group_chore_reminder_job.rb
+++ app/jobs/group_chore_reminder_job.rb
@@ -5,7 +5,7 @@ class GroupChoreReminderJob < ActiveJob::Base
     chore.house.mates.each do |mate|
       notification = mate.notifications.find_by(chore_id: chore.id)
       if notification.email?
-        # MateMailer.chore_notification(chore, mate).deliver_later
+        MateMailer.chore_notification(chore, mate).deliver_later
       end
       if notification.sms?
         chore.sms_reminder(mate)
