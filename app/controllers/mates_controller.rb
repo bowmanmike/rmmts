d3afaf5d3a13de45b240327675558301ed0abef4
karenjho <hokarenj@gmail.com>
2016-03-06 13:18:27 -0500

    Fix order of before_actions in Mates controller

--- app/controllers/mates_controller.rb
+++ app/controllers/mates_controller.rb
@@ -1,7 +1,7 @@
 class MatesController < ApplicationController
-  before_action :load_house
   before_action :load_mate, only: [:show, :edit, :update, :destroy]
   before_action :load_mate_notifications, only: [:show]
+  before_action :load_house
 
   def index
     @mates = Mate.all
