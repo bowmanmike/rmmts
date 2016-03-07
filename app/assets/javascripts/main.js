56c5223f099b8e0be02f70a958414eaeeb35532e
Mike Bowman <mbowman2@gmail.com>
2016-03-07 17:14:41 -0500

    Add popup and overlay to appear and disappear on clicks

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,18 @@
+$(document).on('ready page:load', function() {
+  $(document).on('click', '#login-link', function(e) {
+    e.preventDefault();
+
+    $('#overlay').addClass('is-active');
+    $('.popup-form').addClass('is-active');
+  })
+
+
+  $('html').on('click', '.popup-form', function(e) {
+    e.stopPropagation();
+  })
+
+  $('html').on('click', function() {
+    $('.popup-form').removeClass('is-active');
+    $('#overlay').removeClass('is-active');
+  });
+})
