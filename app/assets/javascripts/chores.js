1d50fc8918502026a3616ae253f496b6c60511ba
karenjho <hokarenj@gmail.com>
2016-03-08 19:50:17 -0500

    Hide and show notification settings with the notification settings icon

--- app/assets/javascripts/chores.js
+++ app/assets/javascripts/chores.js
@@ -8,4 +8,18 @@ $(document).on('ready page:load', function() {
   $('.check-mark').on('click', function() {
     $(this).toggleClass('is-checked');
   });
+
+  $('.show-notification-options').on('click', function(e) {
+    e.preventDefault();
+
+    $(this).parents('.chore').addClass('hide-card');
+    $(this).parents('.chore').siblings('.notification-settings').removeClass('hide-card');
+  });
+
+  $('.hide-notification-options').on('click', function(e) {
+    e.preventDefault();
+
+    $(this).parents('.notification-settings').addClass('hide-card');
+    $(this).parents('.notification-settings').siblings('.chore').removeClass('hide-card');
+  });
 });
