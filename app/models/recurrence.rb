7881dbfdd65d09ff74a8bf443602ec432142ea03
Mike Bowman <mbowman2@gmail.com>
2016-03-09 22:19:46 -0500

    Style chore form, replace radio buttons with select menu and fix bug in correct_weekday method

--- app/models/recurrence.rb
+++ app/models/recurrence.rb
@@ -1,42 +1,5 @@
 module Recurrence
 
-  # def recurrence_alias(frequency_integer, frequency_unit)
-  #   case frequency_unit
-  #   when "days"
-  #     case frequency_integer
-  #     when 1
-  #       "daily"
-  #     when 2
-  #       "every other day"
-  #     else
-  #       "every #{frequency_integer} days"
-  #     end
-  #   when "weeks"
-  #     case frequency_integer
-  #     when 1
-  #       "every #{week_day}"
-  #     else
-  #       "every #{frequency_integer} weeks"
-  #     end
-  #   when "months"
-  #     case frequency_integer
-  #     when 1
-  #       "on the #{ordinalize(month_day)} of every month"
-  #     when 2
-  #       "every other month, on the #{ordinalize(month_day)}"
-  #     else
-  #       "every #{frequency_integer} months, on the #{ordinalize(month_day)}"
-  #     end
-  #   when "years"
-  #     case frequency_integer
-  #     when 1
-  #       "every year"
-  #     else
-  #       "every #{frequency_integer} years"
-  #     end
-  #   end
-  # end
-
   def frequency
     case self.frequency_unit
     when "days"
@@ -51,7 +14,7 @@ module Recurrence
   end
 
   def correct_weekday
-    if self.frequency_weekday != nil
+    if self.frequency_weekday.present?
       unless self.due_date.strftime("%A") == self.frequency_weekday
         weekday_sym = self.frequency_weekday.downcase.to_sym
         new_due_date = due_date.end_of_week(weekday_sym).advance(days: 1)
