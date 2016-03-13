9337c2e8d3cc689e3ae6ba9c4a74cc62505df112
Patrick Cruikshank <patrick.cruikshank@gmail.com>
2016-03-13 15:11:03 -0400

    can submit forms as ajax with enter keypress

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -3,11 +3,21 @@ $(document).on('ready page:load page:change', function() {
   $(document).on('page:change', function() {
     $(document).on('keypress', function(e) {
       if ( $('.popup-form').hasClass('is-active') ) {
+        var form = $('.is-active').find('form');
         if ( e.which == 13 && !e.shiftKey) {
           e.preventDefault();
           // alert("i was pressed");
-          var form = document.getElementsByClassName('popup-form is-active')[0].children[0].getElementsByTagName('form')[0];
-          form.submit();
+          // var form = document.getElementsByClassName('popup-form is-active')[0].children[0].getElementsByTagName('form')[0];
+          // form.submit();
+          $.ajax({
+            url: form.attr('action'),
+            method: form.attr('method'),
+            dataType: 'script',
+            data: form.serialize(),
+            success: function() {
+              $('.popup-form').add('#overlay').add('.popup').removeClass('is-active');
+            }
+          });
         }
       }
     });
