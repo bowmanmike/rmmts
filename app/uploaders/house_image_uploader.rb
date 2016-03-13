16b4a5c037d31d5d79039d5535de0e3206ad2a00
Mike Bowman <mbowman2@gmail.com>
2016-03-13 15:31:47 -0400

    Cloudinary works

--- app/uploaders/house_image_uploader.rb
+++ app/uploaders/house_image_uploader.rb
@@ -8,7 +8,7 @@ class HouseImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
