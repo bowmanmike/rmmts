051b2f36f7bd4c8134ba7d24ffe5967a2ba4180a
karenjho <hokarenj@gmail.com>
2016-03-14 10:20:25 -0400

    Write method to set point attributes after build

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,3 +1,23 @@
 class Point < ActiveRecord::Base
   belongs_to :mate
+
+  def point_attributes(item)
+    self.name = item.name
+    self.due_date = item.due_date
+    self.completed_date = item.updated_at
+    self.category = item.class.name.demodulize
+    self.category_id = item.id
+
+    case self.category
+    when "Chore"
+      self.amount = 3
+    when "Expense"
+      self.amount = 5
+    when "Purchase"
+      self.amount = 2
+    else
+      self.amount = 0
+    end
+  end
+
 end
