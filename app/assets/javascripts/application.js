377c432e804c90d41da6530f845bddcf6982279a
Patrick Cruikshank <patrick.cruikshank@gmail.com>
2016-03-14 14:42:20 -0400

    two-way drag and drop for marking chores complete/incomplete, needs styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,5 @@
 //= require main
 //= require houses
 //= require typeahead.bundle.js
+//= require jquery-ui
 //= require_tree .
--- app/assets/javascripts/chores.js
+++ app/assets/javascripts/chores.js
@@ -2,6 +2,40 @@
 // All this logic will automatically be available in application.js.
 $(document).on('ready page:load', function() {
 
+  initializeDragAndDrop();
+
+  $(document).ajaxSuccess(function() {
+    initializeDragAndDrop();
+  });
+
+  function initializeDragAndDrop() {
+    $('.incomplete-chores-list > .chore').draggable({
+      containment: '.chores-full-container',
+      snap: '.card-list',
+    });
+
+    $('.complete-chores-list > .chore').draggable({
+      containment: '.chores-full-container',
+      snap: '.card-list',
+    })
+
+    $('.incomplete-chores-list').droppable({
+      accept: '.complete-chores-list > .chore',
+      drop: choreDropEvent
+    })
+
+    $('.complete-chores-list').droppable({
+      accept: '.incomplete-chores-list > .chore',
+      drop: choreDropEvent
+    });
+  }
+
+  function choreDropEvent(event, ui) {
+    var chore = ui.draggable;
+    var checkmark = chore.find('.check-mark');
+    checkmark.click();
+  }
+
   $(document).on('change', '#chore_recurring', function() {
     $('.recurrence').toggle()
   });
