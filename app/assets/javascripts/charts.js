873db252a26daf73ac0afcf226ed912e057c04d7
karenjho <hokarenj@gmail.com>
2016-03-18 00:17:23 -0400

    Add points ranking table

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -6,34 +6,6 @@ $(document).on('ready page:load', function() {
       var pointsData = [];
       var points = stats.points;
 
-      var chartColor = function(num) {
-        if (num == 0)
-            return "#76FF03";
-        if (num == 1)
-            return "#00E676";
-        if (num == 2)
-            return "#1DE9B6";
-        if (num == 3)
-            return "#00E5FF";
-        if (num == 4)
-            return "#00B0FF";
-        if (num == 5)
-            return "#2979FF";
-        if (num == 6)
-            return "#651FFF";
-        if (num == 7)
-            return "#D500F9";
-        if (num == 8)
-            return "#F50057";
-        if (num == 9)
-            return "#FF1744";
-      };
-
-      var cvs = $('.test-chart');
-      var newWidth = cvs.parent().width();
-      var newCvs = cvs.width(newWidth).height(200);
-      var ctx = newCvs.get(0).getContext("2d");
-
       for (i = 0; i < points.length; i++) {
         pointsData.push({
             value: points[i].value,
@@ -42,6 +14,7 @@ $(document).on('ready page:load', function() {
         });
       };
 
+      var ctx = $('.points-pie').get(0).getContext("2d");
       new Chart(ctx).Pie(pointsData, {
         segmentShowStroke: false,
         percentageInnerCutout: 70
@@ -52,6 +25,31 @@ $(document).on('ready page:load', function() {
 
   if ($(location).attr('href').search('stats') >= 0) {
     showPointsChart();
+  };
+
+  var chartColor = function(num) {
+    // From Google's Material Design colour palette
+    // http://www.google.com/design/spec/style/color.html#color-color-palette
+    if (num == 0)
+        return "#76FF03";
+    if (num == 1)
+        return "#00E676";
+    if (num == 2)
+        return "#1DE9B6";
+    if (num == 3)
+        return "#00E5FF";
+    if (num == 4)
+        return "#00B0FF";
+    if (num == 5)
+        return "#2979FF";
+    if (num == 6)
+        return "#651FFF";
+    if (num == 7)
+        return "#D500F9";
+    if (num == 8)
+        return "#F50057";
+    if (num == 9)
+        return "#FF1744";
   };
 
 });
