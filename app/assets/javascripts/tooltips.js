9b870297b6f291e2bf565efe6d8521f3bfc0a47d
Patrick Cruikshank <patrick.cruikshank@gmail.com>
2016-03-20 15:08:13 -0400

    render interactive points pie chart into tooltip for house stats link

--- app/assets/javascripts/tooltips.js
+++ app/assets/javascripts/tooltips.js
@@ -25,23 +25,78 @@ $(document).on('ready page:load', function() {
       animation: 'grow',
       contentAsHTML: true,
       interactive: true,
-      minWidth: '400',
+      minWidth: '200',
       position: 'bottom',
       content: 'loading...',
       // function to fire before tooltip opens - load the points-pie div into the tooltip?
-      functionBefore: function(origin, continueTooltip) {
-        continueTooltip();
+      // functionBefore: function(origin, continueTooltip) {
+      //   continueTooltip();
+      //   $.ajax({
+      //     url: origin.attr('href'),
+      //     success: function(data) {
+      //       var canvas = $($.parseHTML(data)).find('canvas.points-pie').clone();
+      //       return origin.tooltipster('content', canvas);
+      //     }
+      //   });
+      // },
+      // function to fire when tooltip and content is added to the dom, run the showPointsChart
+      // function to populate the tooltip with the pie chart from the stats page.
+      functionReady: function(origin, tooltip) {
+        // retrieve and format the JSON data needed for the chart
         $.ajax({
           url: origin.attr('href'),
           success: function(data) {
-            var chart = $($.parseHTML(data)).find('.points-pie').clone();
-            return origin.tooltipster('content', chart);
+            var canvas = $($.parseHTML(data)).find('.points-pie').clone();
+            return origin.tooltipster('content', canvas);
           }
         });
-      },
-      // function to fire when tooltip and content is added to the dom, run the showPointsChart
-      // function to populate the tooltip with the pie chart from the stats page.
 
+        $.getJSON(origin.attr('href') + '.json').done(function(stats){
+          var pointsData = [];
+          var points = stats.points;
+
+          for (var i = 0; i < points.length; i++) {
+            pointsData.push({
+                value: points[i].value,
+                label: points[i].label,
+                color: chartColor(i)
+            });
+          };
+
+          var ctx = $('.points-pie').get(0).getContext("2d");
+          new Chart(ctx).Pie(pointsData, {
+            segmentShowStroke: false,
+            percentageInnerCutout: 70
+          });
+        });
+        // Chart colour definition
+        var chartColor = function(num) {
+          // From Google's Material Design colour palette
+          // http://www.google.com/design/spec/style/color.html#color-color-palette
+          if (num == 0)
+              return "#1DE9B6";
+          if (num == 1)
+              return "#00E676";
+          if (num == 2)
+              return "#76FF03";
+          if (num == 3)
+              return "#C6FF00";
+          if (num == 4)
+              return "#FFEA00";
+          if (num == 5)
+              return "#FFC400";
+          if (num == 6)
+              return "#FF9100";
+          if (num == 7)
+              return "#FF3D00";
+          if (num == 8)
+              return "#FF1744";
+          if (num == 9)
+              return "#F50057";
+          if (num == 10)
+              return "#D500F9";
+        };
+      }
     });
 
 
