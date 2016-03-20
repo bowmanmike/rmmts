79cbb562c685a10e6f58d439ba26c9110c5cbc98
Patrick Cruikshank <patrick.cruikshank@gmail.com>
2016-03-20 14:22:42 -0400

    got dom object .points-pie to load into tooltip

--- app/assets/javascripts/tooltips.js
+++ app/assets/javascripts/tooltips.js
@@ -1,23 +1,49 @@
 $(document).on('ready page:load', function() {
 
   $(document).ajaxSuccess(function() {
+    // tooltip for all links with class tooltip
     $('.tooltip').tooltipster({
       theme: 'tooltipster-punk',
       animation: 'grow',
       contentAsHTML: true,
       interactive: true,
-      minWidth: '300',
+      minWidth: '400',
       functionInit: function(origin, content) {
         $.ajax({
           url: origin.attr('href'),
-          dataType: 'HTML',
+          dataType: 'html',
           success: function(data) {
             return origin.tooltipster('content', data);
           }
         });
       }
     });
+
+    // tooltip for the stats page link
+    $('.link-to-stats > a').tooltipster({
+      theme: 'tooltipster-punk',
+      animation: 'grow',
+      contentAsHTML: true,
+      interactive: true,
+      minWidth: '400',
+      position: 'bottom',
+      content: 'loading...',
+      // function to fire before tooltip opens - load the points-pie div into the tooltip?
+      functionBefore: function(origin, continueTooltip) {
+        continueTooltip();
+        $.ajax({
+          url: origin.attr('href'),
+          success: function(data) {
+            var chart = $($.parseHTML(data)).find('.points-pie').clone();
+            return origin.tooltipster('content', chart);
+          }
+        });
+      },
+      // function to fire when tooltip and content is added to the dom, run the showPointsChart
+      // function to populate the tooltip with the pie chart from the stats page.
+
+    });
+
+
   });
-
-
 });
