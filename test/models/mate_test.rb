3fa5a1926f481688141fe9bf426b6f30d8456cb7
Mike Bowman <mbowman2@gmail.com>
2016-03-21 16:53:09 -0400

    comment out assign_notifications test

--- test/models/mate_test.rb
+++ test/models/mate_test.rb
@@ -1,5 +1,16 @@
 require 'test_helper'
 
 class MateTest < ActiveSupport::TestCase
-  
+  def setup
+  end
+
+  # test 'assign_notifications' do
+  #   mate = create(:mate)
+  #   chore = create(:chore)
+  #   chore.mate_id = mate.id
+  #   chore.house_id = mate.house.id
+  #   mate.assign_notifications
+  #   assert mate.notifications.size > 0
+  # end
+
 end
