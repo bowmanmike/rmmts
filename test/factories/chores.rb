10f9ae3b115dfb769346a8934f24d1fa6f8e4f8e
Mike Bowman <mbowman2@gmail.com>
2016-03-21 11:25:41 -0400

    chore testing

--- test/factories/chores.rb
+++ test/factories/chores.rb
@@ -6,46 +6,9 @@ FactoryGirl.define do
     frequency_integer 1
     # mate
     house
+
+    factory :weekly_chore, class: Chore do
+      frequency_unit "weeks"
+    end
   end
 end
-
-# FactoryGirl.define do
-#   factory :daily_chore, class: "Chore"  do
-#     name "Daily Chore"
-#     due_date "2016-03-27"
-#     frequency_unit "days"
-#     frequency_integer 1
-#     complete true
-#     recurring true
-#   end
-#
-#   factory :weekly_chore, class: "Chore" do
-#     name "Weekly Chore"
-#     due_date "2016-03-27"
-#     frequency_unit "weeks"
-#     frequency_integer 1
-#   end
-#
-#   factory :monthly_chore, class: "Chore" do
-#     name "Monthly Chore"
-#     due_date "2016-03-27"
-#     frequency_unit "months"
-#     frequency_integer 1
-#   end
-#
-#   factory :odd_time_chore, class: "Chore" do
-#     name "Non-Standard Time Chore"
-#     due_date "2016-03-27"
-#     frequency_unit "days"
-#     frequency_integer "20"
-#   end
-#
-#   factory :one_time_chore, class: "Chore" do
-#     name "One-Time Chore"
-#     due_date "2016-03-27"
-#     frequency_unit "days"
-#     frequency_integer 1
-#     recurring false
-#     complete true
-#   end
-# end
--- test/models/chore_test.rb
+++ test/models/chore_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class ChoreTest < ActiveSupport::TestCase
   setup do
     @chore = create(:chore, complete: true)
-    @daily_chore = create(:chore, complete: true)
+    @daily_chore = create(:chore)
+    @weekly_chore = create(:weekly_chore)
   end
 
   test 'check_status_resets_completion' do
@@ -12,48 +13,17 @@ class ChoreTest < ActiveSupport::TestCase
   end
 
   test 'check_status_update_due_date_daily' do
-    @due_date = @daily_chore.due_date
+    due_date = @daily_chore.due_date
     @daily_chore.update_due_date
-    assert (@due_date + 1.days) == @daily_chore.due_date
+    assert (due_date + 1.days) == @daily_chore.due_date
+  end
+
+  test 'check_status_update_due_date_weekly' do
+    due_date = @weekly_chore.due_date
+    @weekly_chore.update_due_date
+    assert (due_date + 1.weeks) == @weekly_chore.due_date
   end
 
 
-  # setup do
-  #   @daily = FactoryGirl.create(:daily_chore)
-  #   @weekly = FactoryGirl.create(:weekly_chore)
-  #   @monthly = FactoryGirl.create(:monthly_chore)
-  #   @odd_time = FactoryGirl.create(:odd_time_chore)
-  #   @one_time = FactoryGirl.create(:one_time_chore)
-  # end
-  #
-  # test "daily_chore_update_due_date" do
-  #   @daily.update_due_date
-  #   assert @daily.due_date == "2016-03-28"
-  # end
-  #
-  # test "weekly_chore_update_due_date" do
-  #   @weekly.update_due_date
-  #   assert @weekly.due_date == "2016-04-03"
-  # end
-  #
-  # test "monthly_chore_update_due_date" do
-  #   @monthly.update_due_date
-  #   assert @monthly.due_date == "2016-04-27"
-  # end
-  #
-  # test "odd_time_update_due_date" do
-  #   @odd_time.update_due_date
-  #   assert @odd_time.due_date == "2016-04-16"
-  # end
-  #
-  # test "check_status_on_one_time_chore" do
-  #   @one_time.check_status
-  #   assert @one_time.complete? && @one_time.due_date == "2016-03-27"
-  # end
-  #
-  # test "check_status_on_recurring_chore" do
-  #   @daily.check_status
-  #   assert !@daily.complete? && @daily.due_date == "2016-03-28"
-  # end
-  #
+
 end
