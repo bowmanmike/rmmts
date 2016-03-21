63101dbac7d03831891f45dc1516f4f80a9ef740
Mike Bowman <mbowman2@gmail.com>
2016-03-21 11:43:13 -0400

    points causing errors in chore tests

--- test/factories/chores.rb
+++ test/factories/chores.rb
@@ -1,14 +1,23 @@
 FactoryGirl.define do
   factory :chore do
     name "Chore Factory"
-    due_date { Time.new.tomorrow }
+    due_date { Time.now.next_week }
     frequency_unit "days"
     frequency_integer 1
-    # mate
+    mate
     house
 
     factory :weekly_chore, class: Chore do
       frequency_unit "weeks"
     end
+
+    factory :monthly_chore, class: Chore do
+      frequency_unit "months"
+    end
+
+    factory :yearly_chore, class: Chore do
+      frequency_unit "years"
+    end
+
   end
 end
--- test/models/chore_test.rb
+++ test/models/chore_test.rb
@@ -5,6 +5,8 @@ class ChoreTest < ActiveSupport::TestCase
     @chore = create(:chore, complete: true)
     @daily_chore = create(:chore)
     @weekly_chore = create(:weekly_chore)
+    @monthly_chore = create(:monthly_chore)
+    @yearly_chore = create(:yearly_chore)
   end
 
   test 'check_status_resets_completion' do
@@ -22,6 +24,18 @@ class ChoreTest < ActiveSupport::TestCase
     due_date = @weekly_chore.due_date
     @weekly_chore.update_due_date
     assert (due_date + 1.weeks) == @weekly_chore.due_date
+  end
+
+  test 'check_status_update_due_date_monthly' do
+    due_date = @monthly_chore.due_date
+    @monthly_chore.update_due_date
+    assert (due_date + 1.months) == @monthly_chore.due_date
+  end
+
+  test 'check_status_update_due_date_yearly' do
+    due_date = @yearly_chore.due_date
+    @yearly_chore.update_due_date
+    assert (due_date + 1.years) == @yearly_chore.due_date
   end
 
 
